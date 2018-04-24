b8ca24d8c834d3b089c4bbf811eb6a2243e5a4d8
Heema Rajkarnikar <hrajkarnikar@hrajkarnikar-macmini.ny.millenniumweb.com>
2018-04-24 06:02:28 -0400

    Initial Commit

---
+++ Notes Reminder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notes Reminder
+//
+//  Created by Heema Rajkarnikar on 4/24/18.
+//  Copyright © 2018 Heema Rajkarnikar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
