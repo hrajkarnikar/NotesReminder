4536ffe04e409e5db3481643ba54f946c64126c7
joshi <joshi@joshis-mac.local>
2018-05-04 05:01:57 -0400

    pods files

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '9.0'
+
+target 'Notes Reminder' do
+  use_frameworks!
+
+  # Pods for Notes Reminder
+
+pod 'SwipeCellKit'
+
+end
---
+++ Pods/Target Support Files/Pods-Notes Reminder/Pods-Notes Reminder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Notes_Reminder : NSObject
+@end
+@implementation PodsDummy_Pods_Notes_Reminder
+@end
